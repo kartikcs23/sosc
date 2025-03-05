d1e4d5f610aa4a2c4dcd5f5337e50294836de278
Karthik <128124871+kartikcs23@users.noreply.github.com>
2025-03-05 22:22:53 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Weather from './components/Weather'
+
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center w-full min-h-screen overflow-hidden">
+        <Weather App/> 
+      </div>
+    </>
+  )
+}
+
+export default App;
